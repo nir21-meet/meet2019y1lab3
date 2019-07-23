5a073b3eb54ae118d5e493541a2847bf6d9594f2
nir21-meet <nir21@meet.mit.edu>
2019-07-23 05:10:17 -0400

    Stopping here

---
+++ AgeCompareCalculator.py
@@ -0,0 +1,6 @@
+personA=input("first person age ")
+personB=input("second person age ")
+
+ageDifference=int(personA)-int(personB)
+ageDifference=abs (ageDifference)
+print("the age difference is: "+str(ageDifference))
