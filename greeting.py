4a61ef6e0955e90c46b833bf2ac790462f7a97ed
nir21-meet <nir21@meet.mit.edu>
2019-07-23 07:19:01 -0400

    Stopping here

---
+++ greeting.py
@@ -0,0 +1,5 @@
+name=input("what's your name? ")
+print("hello there, "+name.capitalize())
+print("your name is "+str(len(name))+" letters long")
+print("the first letter on your name is : "+name[0]+" and the last letter on your name is : "+name[-1])
+print(name[1:len(name)-1])
---
+++ specielCharacters.py
@@ -0,0 +1,1 @@
+print("Instructor Alex asks, \"What are you learning today?\"\nThe students reply,  'We are learning how to print!'\"We can type print(\"\\Day1\"\)to print out \"Day1\"")
